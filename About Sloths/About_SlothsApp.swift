043b2b05caece191348864664b4e97dfb4744c0f
Andrew Scott Pitblado <apitblad@uwo.ca>
2023-06-12 11:44:14 -0400

    Initial Commit

---
+++ About Sloths/About_SlothsApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_SlothsApp.swift
+//  About Sloths
+//
+//  Created by Andrew Pitblado on 2023-06-12.
+//
+
+import SwiftUI
+
+@main
+struct About_SlothsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About Sloths/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  About Sloths
+//
+//  Created by Andrew Pitblado on 2023-06-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
